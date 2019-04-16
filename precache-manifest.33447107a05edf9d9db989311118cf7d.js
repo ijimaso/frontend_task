5538928760498ba37edc238ba287fc9658fc457b
ijimaso <soxup0708@gmail.com>
2019-04-16 20:57:47 +0900

    Updates

---
+++ precache-manifest.33447107a05edf9d9db989311118cf7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55470cfe99fd815cdd7207fcb7d29ad4",
+    "url": "/ijimaso/frontend_task/static/media/default.55470cfe.jpg"
+  },
+  {
+    "revision": "85dbeb5d80dc27b7b2ef",
+    "url": "/ijimaso/frontend_task/static/js/runtime~main.edb183b0.js"
+  },
+  {
+    "revision": "dd1f2ed11e9241f76d86",
+    "url": "/ijimaso/frontend_task/static/js/main.e9192cf7.chunk.js"
+  },
+  {
+    "revision": "a90c9eea2aee7a8cc52b",
+    "url": "/ijimaso/frontend_task/static/js/2.2c0cfbc3.chunk.js"
+  },
+  {
+    "revision": "dd1f2ed11e9241f76d86",
+    "url": "/ijimaso/frontend_task/static/css/main.fd3c4756.chunk.css"
+  },
+  {
+    "revision": "175c345bb0fd04bb2185f2c014cb4a73",
+    "url": "/ijimaso/frontend_task/index.html"
+  }
+];
