891ebcee62591e219c9a9dc5bc72fb4050235986
ijimaso <soxup0708@gmail.com>
2019-04-25 00:01:26 +0900

    Change undone icon

--- src/Todo.js
+++ src/Todo.js
@@ -4,31 +4,44 @@ import CardContent from "@material-ui/core/CardContent";
 import Tooltip from "@material-ui/core/Tooltip";
 import Fab from "@material-ui/core/Fab";
 import Done from "@material-ui/icons/Done";
+import Clear from "@material-ui/icons/Clear";
 import DeleteIcon from "@material-ui/icons/Delete";
 import "./css/Todo.css";
 
 const Todo = (props) => {
-    const todoIdName = props.done ? "done" : "undone";
-    const todoSwitchTooltip = props.done ? "元に戻す" : "完了";
+    const {content,done,switchDone,deleteTodo} = props;
+    const todoIdName = done ? "done" : "undone";
+    const todoSwitchTooltip = done ? "元に戻す" : "完了";
     return (
         <div className="todo">
             <Card>
                 <CardContent>
-                    <p id={todoIdName}>{props.content}</p>
-                    <Tooltip title={todoSwitchTooltip}>
-                        <Fab color="secondary" aria-label="Check" size="small" id="checkicon"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                props.switchDone(props);
-                            }}>
-                            <Done />
-                        </Fab>
-                    </Tooltip>
+                    <p id={todoIdName}>{content}</p>
+                    {done ?
+                        <Tooltip title={todoSwitchTooltip}>
+                            <Fab color="secondary" aria-label="Clear" size="small" id="icons"
+                                onClick={(event) => {
+                                    event.preventDefault();
+                                    switchDone(props);
+                                }}>
+                                <Clear />
+                            </Fab>
+                        </Tooltip> :
+                        <Tooltip title={todoSwitchTooltip}>
+                            <Fab color="secondary" aria-label="Check" size="small" id="icons"
+                                onClick={(event) => {
+                                    event.preventDefault();
+                                    switchDone(props);
+                                }}>
+                                <Done />
+                            </Fab>
+                        </Tooltip>
+                    }
                     <Tooltip title="削除">
                         <Fab aria-label="Delete" size="small"
                             onClick={(event) => {
                                 event.preventDefault();
-                                props.deleteTodo(props);
+                                deleteTodo(props);
                             }}>
                             <DeleteIcon />
                         </Fab>
